3f78ddf73c20ddbe331578be644afb8178ad945e
TanaTanoi <tana.tanoi@powershop.co.nz>
2016-05-14 18:03:48 +1200

    Add image to notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,18 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :stock
+
+  def to_json
+    {
+      "id" => id,
+      "user_id" => user_id,
+      "stock_id" => stock_id,
+      "description" => description,
+      "title" => title,
+      "seen" => seen,
+      "title" => title,
+      "pushed" => pushed,
+      "image" => stock.photos.first.image.url(:thumb)
+    }
+  end
 end
