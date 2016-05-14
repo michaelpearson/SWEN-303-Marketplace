c03027af67ecc9e1be3c9c7af66fd41ae9de1004
TanaTanoi <tana.tanoi@powershop.co.nz>
2016-05-14 23:44:55 +1200

    Changed  results algorithm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    stock =  Stock.all
+    stock   = Stock.all
     @recent = stock.reverse.take(5)
-    @close = stock.reject { |s| s.bid_count < s.price * 0.5 }
-      .reject(&:met_required_bids?)
-      .sort {|a,b| b.percentage_complete <=> a.percentage_complete }
+    @close  = stock.sort_by { |s| s.price.to_f / s.bid_count.to_f }
+      .reject(&:met_required_bids?).first(5)
   end
-
 end
