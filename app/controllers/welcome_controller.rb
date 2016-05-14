c6719333904119fa1d64f3b09496a3d8b465309b
David Barnett <david@barnett.net.nz>
2016-05-14 16:48:27 +1200

    start of welcome page with recent and completing soon
    rename Stock's bids_count => bid_count

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @test = 'Hello world'
+    stock =  Stock.all
+    @recent = stock.reverse.take(5)
+    @close = stock.reject { |s| s.bid_count < s.price * 0.9 }
   end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,12 +3,12 @@ class Stock < ApplicationRecord
   has_many :photos
   accepts_nested_attributes_for :photos
 
-  def bids_count
+  def bid_count
     transactions.count
   end
 
   def met_required_bids?
-    bids_count >= price
+    bid_count >= price
   end
 
   def participating_users
