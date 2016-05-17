cebf06548c9930aa72d82859344bb460b6e80a50
TanaTanoi <tana.tanoi@powershop.co.nz>
2016-05-17 16:41:25 +1200

    Add Lorem Ipsum to seed descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+LOREM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 users = User.create([
   { username:"admin", realname:"Sally Smith",password:"1337", token_count: rand(0..10)},
@@ -23,24 +23,24 @@ users = User.create([
 
 stocks = []
 [
-  [users[1], "Camera",  12, ["camera.jpg"], "This is a description for Camera"],
-  [users[1], "Hat",  30, ["hat.jpg"], "This is a description for Hat"],
-  [users[2], "Bald Eagle",  35, ["bald-eagle.jpg"], "This is a description for Bald Eagle"],
-  [users[2], "Kiwi",  80, ["kiwi.jpg"], "This is a description for Kiwi"],
-  [users[3], "Snakes and Ladders",  1, ["snakes-ladders.jpg"], "This is a description for Snakes and Ladders"],
-  [users[3], "Monopoly",  3, ["monopoly.jpg"], "This is a description for Monopoly"],
-  [users[4], "Holy Grail",  100, ["holy-grail.jpg"], "This is a description for Holy Grail"],
-  [users[5], "Meaning of Life",  42, ["meaning-of-life.jpg"], "This is a description for Meaning of Life"],
-  [users[5], "Cactus",  15, ["cactus.jpg"], "This is a description for Cactus"],
-  [users[5], "Iris",  8, ["iris.jpg"], "This is a description for Iris"],
-  [users[6], "Knives",  10, ["knives-1.jpg", "knives-2.jpg", "knives-3.jpg", "knives-4.jpg"], "This is a description for Knives"],
-  [users[6], "Sword",  35, ["sword.jpg"], "This is a description for Sword"],
-  [users[7], "Kryptonite",  10, ["kryptonite.jpg"], "This is a description for Kryptonite"],
-  [users[7], "S Clothing Patch",  5, ["s-patch.jpg"], "This is a description for S Clothing Patch"],
-  [users[8], "Table",  10, ["table.jpg"], "This is a description for Table"],
-  [users[9], "Small Chair",  30, ["chair.jpg"], "This is a description for Small Chair"],
-  [users[10], "Coffee",  4, ["coffee.jpg"], "This is a description for Coffee"],
-  [users[10], "Muffin",  3, ["muffin.jpg"], "This is a description for Muffin"]
+  [users[1], "Camera",  12, ["camera.jpg"], "This is a description for Camera. #{LOREM}"],
+  [users[1], "Hat",  30, ["hat.jpg"], "This is a description for Hat. #{LOREM}"],
+  [users[2], "Bald Eagle",  35, ["bald-eagle.jpg"], "This is a description for Bald Eagle. #{LOREM}"],
+  [users[2], "Kiwi",  80, ["kiwi.jpg"], "This is a description for Kiwi. #{LOREM}"],
+  [users[3], "Snakes and Ladders",  1, ["snakes-ladders.jpg"], "This is a description for Snakes and Ladders. #{LOREM}"],
+  [users[3], "Monopoly",  3, ["monopoly.jpg"], "This is a description for Monopoly. #{LOREM}"],
+  [users[4], "Holy Grail",  100, ["holy-grail.jpg"], "This is a description for Holy Grail. #{LOREM}"],
+  [users[5], "Meaning of Life",  42, ["meaning-of-life.jpg"], "This is a description for Meaning of Life. #{LOREM}"],
+  [users[5], "Cactus",  15, ["cactus.jpg"], "This is a description for Cactus. #{LOREM}"],
+  [users[5], "Iris",  8, ["iris.jpg"], "This is a description for Iris. #{LOREM}"],
+  [users[6], "Knives",  10, ["knives-1.jpg", "knives-2.jpg", "knives-3.jpg", "knives-4.jpg"], "This is a description for Knives. #{LOREM}"],
+  [users[6], "Sword",  35, ["sword.jpg"], "This is a description for Sword. #{LOREM}"],
+  [users[7], "Kryptonite",  10, ["kryptonite.jpg"], "This is a description for Kryptonite. #{LOREM}"],
+  [users[7], "S Clothing Patch",  5, ["s-patch.jpg"], "This is a description for S Clothing Patch. #{LOREM}"],
+  [users[8], "Table",  10, ["table.jpg"], "This is a description for Table. #{LOREM}"],
+  [users[9], "Small Chair",  30, ["chair.jpg"], "This is a description for Small Chair. #{LOREM}"],
+  [users[10], "Coffee",  4, ["coffee.jpg"], "This is a description for Coffee. #{LOREM}"],
+  [users[10], "Muffin",  3, ["muffin.jpg"], "This is a description for Muffin. #{LOREM}"]
 ].each do |stock| 
   s = Stock.create(
     owner: stock[0],
