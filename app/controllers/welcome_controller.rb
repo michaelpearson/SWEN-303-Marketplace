893b9ef269bc91e6780676fef83fe6aae21030d9
David Barnett <david@barnett.net.nz>
2016-05-20 15:59:04 +1200

    start of featured raffle page and partilalized carousel

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
 
   def index
     stock   = Stock.all
-    @recent = stock.reverse.take(5)
-    @close  = stock.sort_by { |s| s.price.to_f / s.bid_count.to_f }
-      .reject(&:met_required_bids?).first(5)
+    @featured = stock.select {|s| s.photos.length > 1 }.first
   end
 end
