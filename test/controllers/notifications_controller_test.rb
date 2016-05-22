7b139a37d7b40a968b7a9a45d7d913ba55b81073
Michael <zl2mjp@gmail.com>
2016-05-22 19:20:48 +1200

    Removed those tests we definitely wrote & added serve file config for heroku

--- test/controllers/notifications_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get push" do
-    get notifications_push_url
-    assert_response :success
-  end
-
-end
